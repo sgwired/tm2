2b19b8319eb0b09e640e5dbf00e95551b0294dcf
shelton gayles <=sg.bythelake@gmail.com>
2018-08-17 17:22:29 -0700

    finished profile page

--- app/models.py
+++ app/models.py
@@ -9,6 +9,19 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     toys = db.relationship('Toy', backref='maker', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    address1 = db.Column(db.String(255))
+    address2 = db.Column(db.String(255))
+    city = db.Column(db.String(255))
+    state =  db.Column(db.Integer, db.ForeignKey('state.id'))
+    postal_code = db.Column(db.String(255))
+    phone = db.Column(db.String(255))
+    website = db.Column(db.String(255))
+    facebook = db.Column(db.String(255))
+    twitter = db.Column(db.String(255))
+    instagram = db.Column(db.String(255))
+    company = db.Column(db.String(255))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -32,3 +45,11 @@ class Toy(db.Model):
     def __repr__(self):
         return '<Toy {}>'.format(self.body)
 
+
+class State(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<State {}>'.format(self.body)
