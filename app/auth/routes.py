a317ce6fc13ff355dfd343cf15bf48da5c8cd3e4
shelton gayles <=sg.bythelake@gmail.com>
2018-08-21 10:42:34 -0700

    changed user login to go to user page

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -19,7 +19,7 @@ def before_request():
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('main.homepage'))
+        return redirect(url_for('main.user', username=current_user.username))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -29,7 +29,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('main.homepage')
+            next_page = url_for('main.user', username=current_user.username)
         return redirect(next_page)
     return render_template('auth/login.html', title='Sign In', form=form)
 
@@ -37,13 +37,13 @@ def login():
 @bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('main.homepage'))
+    return redirect(url_for('main.index'))
 
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('homepage'))
+        return redirect(url_for('main.user', username=current_user.username))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
